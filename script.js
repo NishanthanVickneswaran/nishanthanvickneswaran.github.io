a447c3d0e08059d2cc9f293505a087902f686777
NishanthanVickneswaran <32403307+NishanthanVickneswaran@users.noreply.github.com>
2020-08-14 19:00:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
 var products = [
   {
     name: 'High Flow Aluminum Charge Pipe Kit ',
-    image: 'images/intercooler.jpg',
+    image: 'nishanthanvickneswaran.github.io/images/intercooler.jpg',
     category: 'engine',
     description: 'Increase intake air flow with aluminum charge pipes for your OEM intercooler',
     price: 599.99,
